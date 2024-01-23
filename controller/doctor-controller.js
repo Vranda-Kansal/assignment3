88dcb15fba2e6144a34172363a382f90b42c4abd
Vranda Kansal <itzvaniaggarwal12@gmail.com>
2024-01-23 14:01:52 +0530

    done-done

--- controller/doctor-controller.js
+++ controller/doctor-controller.js
@@ -5,7 +5,8 @@ export const getDoctorList = (req, res) => {
     const selectedCity = req.params.city;
     // console.log(selectedCity);
     const list = DoctorList.filter((doctor) => doctor.city === selectedCity);
-    return res.send(list);
+    console.log(list);
+    return res.status(200).json(list);
   } catch (err) {
     return res.status(500).json(err.message);
   }
