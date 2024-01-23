7f3245629d4c3e7c892633ef44949108c5dacf63
Vranda Kansal <itzvaniaggarwal12@gmail.com>
2024-01-23 14:08:50 +0530

    checkk

--- index.js
+++ index.js
@@ -9,7 +9,17 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
-app.use("/doctor/list/:city", route);
+app.get("/doctor/list/:city", (req, res) => {
+  try {
+    const selectedCity = req.params.city;
+    // console.log(selectedCity);
+    const list = DoctorList.filter((doctor) => doctor.city === selectedCity);
+    console.log(list);
+    return res.status(200).json(list);
+  } catch (err) {
+    return res.status(500).json(err.message);
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
