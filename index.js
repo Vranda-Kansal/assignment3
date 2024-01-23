9e570c4d697dbdf114dce4cdc4994ce5ab71a2b2
Vranda Kansal <itzvaniaggarwal12@gmail.com>
2024-01-23 12:58:54 +0530

    deployment

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ import express from "express";
 
 import cors from "cors";
 import route from "./routes/route.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 
-app.use("/", route);
+app.use("/doctor/list/:city", route);
 
-const PORT = 8000;
+const port = process.env.PORT || 8001;
 
 app.listen(PORT, () => {
-  console.log(`Server is running at ${PORT}`);
+  console.log(`Server is running at ${port}`);
 });
